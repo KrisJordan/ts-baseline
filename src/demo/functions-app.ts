68892586e94b12106dd49f0c14f623afaccb638e
Kris Jordan <krisjordan@gmail.com>
2017-07-10 12:46:02 -0400

    Add functions example.

---
+++ src/demo/functions-app.ts
@@ -0,0 +1,18 @@
+import "introcs/console";
+
+function main() {
+    print("Hello, again.");
+    welcome("TypeScript");
+    partyParrot();
+    print("Ah yeah.");
+}
+
+function welcome(to: string) {
+    print("Welcome to " + to + "!!!");
+}
+
+function partyParrot() {
+    image("http://i.imgur.com/l92fg3z.gif");
+}
+
+main();
