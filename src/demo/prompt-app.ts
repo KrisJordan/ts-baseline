86d4c4876a61418637ae37e27fb29031cc6ff06d
Kris Jordan <krisjordan@gmail.com>
2017-07-12 10:13:27 -0400

    Adding example of prompts and clear

---
+++ src/demo/prompt-app.ts
@@ -0,0 +1,29 @@
+import "introcs/console";
+
+function main() {
+    promptString("What is your name?", handleName);
+}
+
+function handleName(name: string) {
+    clear();
+    print("Hello " + name);
+    promptNumber("How old are you?", handleAge);
+}
+
+function handleAge(age: number) {
+    print("Soon you will be " + (age + 1));
+    promptBoolean("True or False: The Earth is flat.", handleFlatness);
+}
+
+function handleFlatness(flatness: boolean) {
+    clear();
+    if (!flatness) {
+        print("Great work!!!");
+        image("https://media.giphy.com/media/tBb19faQdmuOHmwGRPy/giphy.gif");
+    } else {
+        print("Ummm... try again...");
+        main();
+    }
+}
+
+main();
